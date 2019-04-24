9260dd1aa2a376b559961bc108d1783d428b8709
zxy <236627316@qq.com>
2019-04-25 00:15:33 +0800

    Update 前缀和与差分.cpp

--- 差分/前缀和与差分.cpp
+++ 差分/前缀和与差分.cpp
@@ -16,12 +16,12 @@ int main()
         _for(j, 1, n)
         {
             int x; scanf("%d", &x);
-            a[i][j] = x + a[i-1][j] + a[i][j-1] - a[i-1][j-1];
+            a[i][j] = x + a[i-1][j] + a[i][j-1] - a[i-1][j-1];//前缀和
         }
     
     int x1, y1, x2, y2;
     while(~scanf("%d%d%d%d", &x1, &y1, &x2, &y2))
-        printf("%d\n", a[x2][y2] - a[x1-1][y2] - a[x2][y1-1] + a[x1-1][y1-1]);
+        printf("%d\n", a[x2][y2] - a[x1-1][y2] - a[x2][y1-1] + a[x1-1][y1-1]); //求区域内的和
  
     return 0;
 }
@@ -42,13 +42,15 @@ int main()
     {
         int x1, y1, x2, y2, p;
         scanf("%d%d%d%d%d", &x1, &y1, &x2, &y2, &p);
-        a[x1][y1] += p; a[x2+1][y2+1] += p;
-        a[x2+1][y1] -= p; a[x1][y2+1] -= p;
+        a[x1][y1] += p; 
+        a[x2+1][y1] -= p;
+        a[x1][y2+1] -= p;
+         a[x2+1][y2+1] += p;//减了两次，减多了，加回来
     } 
 
     _for(i, 1, n)
         _for(j, 1, n)
-            a[i][j] += a[i-1][j] + a[i][j-1] - a[i-1][j-1];
+            a[i][j] += a[i-1][j] + a[i][j-1] - a[i-1][j-1];//求差分数组的前缀和
     
     _for(i, 1, n)
     {
